5ace18c394ddfe699c04804435b8a3be4a4e32c2
GirdaViktor <VSGirda@gmail.com>
2021-09-17 11:29:21 +0300

    start project 13

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="header__content wrapper">
+        <a href="#" className="logo">logo</a>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/Main.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Main = () => {
+  return (
+    <main className="main">
+      <div>
+        <img src="" alt=""/>
+      </div>
+      <div>
+        ava + description
+      </div>
+      <div>
+        My posts
+        <div>
+          New post
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default Main;
